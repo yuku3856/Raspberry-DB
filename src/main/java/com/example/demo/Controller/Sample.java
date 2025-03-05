6a3039108d63483406644298065c22a5ca55ea07
yuku <mie515173@gmail.com>
2025-03-05 17:31:15 +0900

    最初のコミット

---
+++ src/main/java/com/example/demo/Controller/Sample.java
@@ -0,0 +1,7 @@
+package com.example.demo.Controller;
+
+public class Sample {
+	public String getMessage() {
+		return "成功だよ！";
+	}
+}
---
+++ src/main/java/com/example/demo/RaspberryDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberryDbApplication.class, args);
+	}
+
+}
